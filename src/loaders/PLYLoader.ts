bbb9abeeae12f742b7991eeb6a04b29b7a0d8681
Alexandre Lanvin <alanvin@ad.inria.fr>
2024-01-04 11:53:05 +0100

    Revert "removing cors when fetching file from url"
    This reverts commit 38a82f41297c3b44236c9f0b047bd6c90c6301a4.

--- src/loaders/PLYLoader.ts
+++ src/loaders/PLYLoader.ts
@@ -17,7 +17,7 @@ class PLYLoader {
         quantized: boolean = false
     ): Promise<void> {
         const req = await fetch(url, {
-            mode: "no-cors",
+            mode: "cors",
             credentials: "omit",
         });
 
